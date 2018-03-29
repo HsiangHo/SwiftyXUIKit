5a23654506e0756f40fb16a3dc92e9d937d25696
HsiangHo <ho_cheung@qq.com>
2018-03-29 19:05:32 +0800

    Initialize workspace

---
+++ SwiftyXUIKit/SwiftyXUIKit/SwiftyXUIKit.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyXUIKit.h
+//  SwiftyXUIKit
+//
+//  Created by Jovi on 3/29/18.
+//  Copyright © 2018 Jovi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftyXUIKit.
+FOUNDATION_EXPORT double SwiftyXUIKitVersionNumber;
+
+//! Project version string for SwiftyXUIKit.
+FOUNDATION_EXPORT const unsigned char SwiftyXUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyXUIKit/PublicHeader.h>
+
+
